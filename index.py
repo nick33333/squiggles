ab3eb7d637b0d4495269a9fa8ba156b5eca6c1eb
nick33333 <nickchan55555@gmail.com>
2023-07-11 02:00:56 -0700

    Added session variables and multi-file uploading feature

--- index.py
+++ index.py
@@ -19,11 +19,13 @@ import os
 from tslearn.clustering import TimeSeriesKMeans
 import plotly
 import plotly.express as px
+from plotly.subplots import make_subplots
 import json
 
 sys.path.insert(1, 'MSMC_clustering/')
 from MSMC_clustering import Msmc_clustering
 from MSMC_plotting import *
+from MSMC_form_kwarg_handler import manual_modify_dict
 
 '''
 To use flask shell:
@@ -35,22 +37,14 @@ UPLOAD_FOLDER = os.path.join('static', 'uploads')
 # Define allowed files
 ALLOWED_EXTENSIONS = {'csv'}
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
-app.secret_key = 'This is your secret key to utilize session in Flask'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-# Hardcoded stuff for now, should allow user to select these settings from a drop bar
-app.config['animal'] = 'bird' # bird and mammal are options
-app.config['preprocessing'] = "msmc_rtp_it" # msmc, msmc_it, and msmc_rtp_it (realtime processed and interpolated) should be my options so far
-app.config['k'] = 7 # k= 2, ..., 15, ... models should be ready
+# init session data
+# session['uploads'] = []
+# session['form_results'] = ''
+# session['fname'] = ''
 
-# Will fill up this dict with some fat models using specified settings
-km_dict = dict()
-km_dict[app.config['k']] = TimeSeriesKMeans.from_pickle(f"models/{app.config['animal']}_{app.config['preprocessing']}_k{app.config['k']}_km.pkl")
-
-# My testing model
-app.config[f"k{app.config['k']}"] = km_dict[app.config['k']] # Should be a tslearn TimeSeriesKMeans obj
+# CONSTANTS
+# Form inputs for Msmc_clustering kwargs which don't require user input
 barred_args = ['self',
                'directory',
                'generation_time_path',
@@ -60,111 +54,142 @@ barred_args = ['self',
                'readfile_kwargs',
                'tmp_data',
                'use_plotting_on_log10_scale']
+# Lists to cast form inputs for Msmc_clustering kwargs into appropriate data
+# types.
+to_int_list = ['interpolation_pts',
+               'manual_cluster_count',
+               'omit_back_prior',
+               'omit_front_prior']
+to_float_list = ['mu']
+to_bool_list = ['use_interpolation',
+                'use_real_time_and_c_rate_transform',
+                'use_time_log10_scaling',
+                'use_value_normalization']
+to_list_list = ['time_window']
+
+
+# App
+
+# App config
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.secret_key = 'This is your secret key to utilize session in Flask'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 Msmc_clustering_args = Msmc_clustering.__init__.__code__.co_varnames
-Msmc_clustering_args = [i for i in Msmc_clustering_args if i not in barred_args]
+Msmc_clustering_args = [i for i in Msmc_clustering_args if i not in
+                        barred_args]
 app.config['Msmc_clustering_args'] = Msmc_clustering_args
+
+
+# Helper functions
+def find_upload_path(data_filename: "str") -> "str":
+    '''
+    Give data_filename and receive path to file. Path will
+    vary depending on app.config['UPLOAD_FOLDER'] and name
+    '''
+    return os.path.join(app.config['UPLOAD_FOLDER'],
+                        data_filename)
+
+
+def clean_uploads(session):
+    '''
+    Given session, remove files uploaded during session.
+    '''
+    if session.get('uploads') is None:
+        return
+    elif isinstance(session.get('uploads'),
+                    list) and len(session.get('uploads')) > 0:
+        for data_filename in session.get('uploads'):
+            path_to_file = find_upload_path(data_filename=data_filename)
+            print(f"Removing {path_to_file}")
+            os.remove(path_to_file)
+        return
+
+
+# App stuff
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    '''
+    Home page gives options to upload data and choose clustering settings
+    '''
     if request.method == 'POST':
+        # Form handling
         session['form_results'] = request.form  # saves form data after upload
-        f = request.files.get('file')  # Extracting uploaded file name
-        data_filename = secure_filename(f.filename)
-        session['fname'] = data_filename
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], data_filename))
-        session['uploaded_data_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], data_filename)
-        return render_template('index2.html', Msmc_clustering_args=Msmc_clustering_args)
-    return render_template("index.html", Msmc_clustering_args=Msmc_clustering_args)
+        # File handling
+        # print(f"request.files {request.files}")
+        if 'file(s)' not in request.files:
+            flash('No file(s) given')
+            return redirect(request.url)
+        files = request.files.getlist('file(s)')  # retrieve list of uploaded fnames
+        for file in files:
+            if file:
+                # print(f"filename: {file.filename}")
+                data_filename = secure_filename(file.filename)
+                # print(f"data_filename: {data_filename}")
+                if session.get('uploads') is None:
+                    session['uploads'] = [data_filename]
+                else:
+                    session['uploads'].append(data_filename)
+                file.save(find_upload_path(data_filename=data_filename))
+                session['path_to_last_upload'] = find_upload_path(data_filename=data_filename)
+        return render_template('index2.html',
+                               Msmc_clustering_args=Msmc_clustering_args)
+    return render_template("index.html",
+                           Msmc_clustering_args=Msmc_clustering_args)
 
 @app.route('/show_data')
 def showData():
-    data_file_path = session.get('uploaded_data_file_path', None)  # Uploaded File Path
+    '''
+    Instead of session.get in data_file_path, I could use a dropdown bar to
+    assign data_file_path for selective plotting of uploaded data using
+    session['uploads'] and find_upload_path.
+    '''
+    data_file_path = session.get('path_to_last_upload', None)  # Uploaded File Path
     uploaded_df = pd.read_csv(data_file_path, encoding='unicode_escape')  # read csv
     uploaded_df_html = uploaded_df.to_html()  # Converting to html Table
-    
-    return render_template('show_csv_data.html', fname=session['fname'], data_var=uploaded_df_html)
+    return render_template('show_csv_data.html',
+                           fname=session['fname'],
+                           data_var=uploaded_df_html)
 
 @app.route('/plot_data')
 def plotData():
-    # Read dataset
-    form_data = request.form
-    print(list(form_data.items()))
-    # Make plotly subplot
-    
+    '''
+    Instead of session.get in data_file_path, I could use a dropdown bar to
+    assign data_file_path for selective plotting of uploaded data using
+    session['uploads'] and find_upload_path.
+    '''
     # Read from Uploaded File Path
-    data_file_path = session.get('uploaded_data_file_path', None)
+    data_file_path = session.get('path_to_last_upload', None)
     # read csv
     uploaded_df = pd.read_csv(data_file_path, encoding='unicode_escape')
     fig = px.line(uploaded_df, x='time', y='NE', title='Muh plot')
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)    
-    
     return render_template('plot_csv_data.html', fname=session['fname'],
                            data_var=uploaded_df, graphJSON=graphJSON)
 
 @app.route('/plot_clusters')
 def plotClusters():
-    def manual_modify_dict(my_dict,
-                           to_int_list,
-                           to_float_list,
-                           to_bool_list,
-                           to_list_list):
-        my_dict = to_int(to_int_list, my_dict)
-        my_dict = to_float(to_float_list, my_dict)
-        my_dict = to_bool(to_bool_list, my_dict)
-        my_dict = to_li(to_list_list, my_dict)
-        return my_dict
-        
-    def to_int(to_int_list, my_dict):
-        for thing in to_int_list:
-            my_dict[thing] = int(my_dict[thing])
-        return my_dict
-    
-    def to_float(to_float_list, my_dict):
-        for thing in to_float_list:
-            my_dict[thing] = float(my_dict[thing])
-        return my_dict
-            
-    def to_bool(to_bool_list, my_dict):
-        for thing in to_bool_list:
-            my_dict[thing] = my_dict[thing] == 'True'
-        return my_dict
 
-    def to_li(to_list_list, my_dict):
-        for thing in to_list_list:
-            umm = thing.split(',')
-            if len(umm) > 1:
-                my_dict[thing] = [float(t) for t in umm]
-                print(my_dict[thing])
-        return my_dict
-
-    to_int_list = ['interpolation_pts',
-                'manual_cluster_count',
-                'omit_back_prior',
-                'omit_front_prior']
-    to_float_list = ['mu']
-    to_bool_list = ['use_interpolation',
-                    'use_real_time_and_c_rate_transform',
-                    'use_time_log10_scaling',
-                    'use_value_normalization']
-    to_list_list = ['time_window']
     # Load form results
     user_settings = session['form_results']
     # CURRENTLY WORKING ON HOW TO READ FREAKING FORM DATA
     print("plotClusters")
-    data_file_path = session.get('uploaded_data_file_path', None)   
+    # data_file_path = session.get('path_to_last_upload', None)
     # user_settings['directory']=data_file_path # Update user_settings file path 
-    non_user_settings = user_settings.copy() # Modify user settings but for base dataset
-    user_settings['directory'] = 'static/uploads/' # Lame way of loading user input (VERY BAD CUZ ALL DATA CAN BE FROM DIFFERENT SESSIONS AND FORMATS)
-    common_settings  = {'generation_time_path':'data/generation_lengths/',
-                        'exclude_subdirs':[],
+    non_user_settings = user_settings.copy()  # Modify user settings but for base dataset
+    user_settings['directory'] = 'static/uploads/'  # Lame way of loading user input (VERY BAD CUZ ALL DATA CAN BE FROM DIFFERENT SESSIONS AND FORMATS)
+    common_settings  = {'generation_time_path': 'data/generation_lengths/',
+                        'exclude_subdirs': [],
                         'use_plotting_on_log10_scale': False,
                         'sep': '\t',
                         'data_file_descriptor': '.csv'}
     user_defaults = common_settings.copy()
-    non_user_settings_to_update = {'directory':'data/msmc_curve_data_birds/',
-                                   'time_field':'left_time_boundary',
-                                   'value_field':'lambda',
-                                   'data_file_descriptor':'.txt'}
+    non_user_settings_to_update = {'directory': 'data/curves_80_20_split_birds/train_data/',
+                                   'time_field': 'left_time_boundary',
+                                   'value_field': 'lambda',
+                                   'data_file_descriptor': '.txt'}
     
     user_defaults.update(user_settings) # Load user settings for user dataset
     
@@ -183,13 +208,16 @@ def plotClusters():
                                        to_list_list=to_list_list)
     print("non_user_settings = ", non_user_settings)
     print("user_settings = ", user_defaults)
-    
+
     m_obj_base = Msmc_clustering(**non_user_settings)
-    
+    '''
+    Might fail to correctly read cols if static/upload/ dir is full of files
+    from other ppl's sessions! Common cause of Usecols do not match error.
+    '''
     m_obj_user = Msmc_clustering(**user_defaults)
     m_obj_base.cluster_curves(plot_everything=False)
     cols = 2
-    k=m_obj_base.manual_cluster_count                 
+    k = m_obj_base.manual_cluster_count              
     rows = given_col_find_row(k=k, cols=cols)
     fig = make_subplots(rows=rows, cols=cols,
                         subplot_titles=([f"Cluster {label}" for label in range(1, k+1)]))
@@ -200,19 +228,22 @@ def plotClusters():
                              cols=cols,
                              Msmc_clustering=m_obj_base,
                              km=None,
-                             marker_color='rgba(0, 180, 255, .5)')
+                             marker_color='rgba(0, 180, 255, .3)')
     # Add curve from user input
     for name in m_obj_user.name2series:
         add_curve_to_subplot(fig=fig,
-                            name=name,
-                            cols=cols,
-                            Msmc_clustering=m_obj_user,
-                            km=m_obj_base.km,
-                            marker_color='rgba(180, 0, 255, .8)')
+                             name=name,
+                             cols=cols,
+                             Msmc_clustering=m_obj_user,
+                             km=m_obj_base.km,
+                             marker_color='rgba(180, 0, 255, .8)')
     fig.update_layout(width=1900,
                       height=1000,)
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)    
-    return render_template('plot_clusters.html', fname=session['fname'], graphJSON=graphJSON)
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    return render_template('plot_clusters.html',
+                           fname=session['fname'],
+                           graphJSON=graphJSON)
 
 
 
